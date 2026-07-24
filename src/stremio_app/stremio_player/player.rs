91a82801a2149b52ae5ee7fa07e3e009b21faf5e
Timothy Z. <timothy@stremio.com>
2026-07-24 13:11:33 +0300

    Update libmpv2 to 6.0.0

--- src/stremio_app/stremio_player/player.rs
+++ src/stremio_app/stremio_player/player.rs
@@ -416,7 +416,7 @@ fn display_hdr_active(path: &DISPLAYCONFIG_PATH_INFO) -> Option<bool> {
 }
 
 fn create_event_thread(
-    mut mpv_event_client: Mpv,
+    mpv_event_client: Mpv,
     observe_property_receiver: Receiver<ObserveProperty>,
     rpc_response_sender: Sender<String>,
 ) -> JoinHandle<()> {
